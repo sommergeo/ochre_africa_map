ad042fa09d1edf62bad0da08a240c429303a5dd8
Hans Wurst <me@uni-tuebingen.de>
2018-07-13 12:28:41 +0200

    insert leaflet

---
+++ data/Occasional310210ka_2.js
@@ -0,0 +1,20 @@
+var json_Occasional310210ka_2 = {
+"type": "FeatureCollection",
+"name": "Occasional310210ka_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "assemblage": "Border Cave\t", "locality.x": 31.98875, "locality.y": -27.021443, "ochre": "12" }, "geometry": { "type": "Point", "coordinates": [ 31.98875, -27.021443 ] } },
+{ "type": "Feature", "properties": { "assemblage": "Canteen Koppie\t", "locality.x": 24.532467, "locality.y": -28.542357, "ochre": "2" }, "geometry": { "type": "Point", "coordinates": [ 24.532467, -28.542357 ] } },
+{ "type": "Feature", "properties": { "assemblage": "Duinefontein (DFT) 2\t", "locality.x": 18.42820278, "locality.y": -33.6649889, "ochre": "1" }, "geometry": { "type": "Point", "coordinates": [ 18.42820278, -33.6649889 ] } },
+{ "type": "Feature", "properties": { "assemblage": "Hoedjiespunt 1\t", "locality.x": 17.9596528, "locality.y": -33.028567, "ochre": "20" }, "geometry": { "type": "Point", "coordinates": [ 17.9596528, -33.028567 ] } },
+{ "type": "Feature", "properties": { "assemblage": "Kapthurin GnJh-15\t", "locality.x": 35.982836, "locality.y": 0.573497, "ochre": "74" }, "geometry": { "type": "Point", "coordinates": [ 35.982836, 0.573497 ] } },
+{ "type": "Feature", "properties": { "assemblage": "Kathu Pan 1\t", "locality.x": 23.0072222, "locality.y": -27.6663889, "ochre": "30" }, "geometry": { "type": "Point", "coordinates": [ 23.0072222, -27.6663889 ] } },
+{ "type": "Feature", "properties": { "assemblage": "Linksfield \/ Primrose Ridge", "locality.x": 28.0, "locality.y": -26.17, "ochre": "2" }, "geometry": { "type": "Point", "coordinates": [ 28.0, -26.17 ] } },
+{ "type": "Feature", "properties": { "assemblage": "Mumbwa\t", "locality.x": 27.01666667, "locality.y": -14.96666667, "ochre": "4" }, "geometry": { "type": "Point", "coordinates": [ 27.01666667, -14.96666667 ] } },
+{ "type": "Feature", "properties": { "assemblage": "Nswatugi\t", "locality.x": 28.475274, "locality.y": -20.535715, "ochre": "3" }, "geometry": { "type": "Point", "coordinates": [ 28.475274, -20.535715 ] } },
+{ "type": "Feature", "properties": { "assemblage": "Pomongwe Cave\t", "locality.x": 28.513502, "locality.y": -20.547533, "ochre": "4" }, "geometry": { "type": "Point", "coordinates": [ 28.513502, -20.547533 ] } },
+{ "type": "Feature", "properties": { "assemblage": "Sai Island 8-B-11\t", "locality.x": 30.32, "locality.y": 20.7, "ochre": "59" }, "geometry": { "type": "Point", "coordinates": [ 30.32, 20.7 ] } },
+{ "type": "Feature", "properties": { "assemblage": "Twin Rivers\t", "locality.x": 28.1833333, "locality.y": -15.5166667, "ochre": "419" }, "geometry": { "type": "Point", "coordinates": [ 28.1833333, -15.5166667 ] } },
+{ "type": "Feature", "properties": { "assemblage": "Wonderwerk\t", "locality.x": 23.553834, "locality.y": -27.846447, "ochre": "12" }, "geometry": { "type": "Point", "coordinates": [ 23.553834, -27.846447 ] } }
+]
+}
